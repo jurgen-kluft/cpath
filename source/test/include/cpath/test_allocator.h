7551b836a8654a993bd0a823ecdce88010754475
Jurgen Kluft2 <jurgen@virtuosgames.com>
2024-08-27 11:06:19 +0800

    * cpath
    * update 'test_allocator.h'

--- source/test/include/cpath/test_allocator.h
+++ source/test/include/cpath/test_allocator.h
@@ -5,7 +5,7 @@
 #    pragma once
 #endif
 
-#include "ccore/c_allocator.h"
+#include "cbase/c_allocator.h"
 #include "cunittest/private/ut_Config.h"
 
 class test_alloc_t : public ncore::alloc_t
@@ -15,12 +15,12 @@ class test_alloc_t : public ncore::alloc_t
 public:
     test_alloc_t(UnitTest::TestAllocator** allocator) : mAllocator(allocator) {}
     virtual void* v_allocate(ncore::u32 size, ncore::u32 align) { return (*mAllocator)->Allocate(size, align); }
-    virtual void  v_deallocate(void* p) { return (*mAllocator)->Deallocate(p); }
+    virtual void  v_deallocate(void* p) { (*mAllocator)->Deallocate(p); }
     virtual void  v_release() {}
 };
 
-#define UNITTEST_ALLOCATOR                         \
-    static test_alloc_t TestAlloc(&TestAllocator); \
+#define UNITTEST_ALLOCATOR                            \
+    static test_alloc_t TestAlloc(&FixtureAllocator); \
     static alloc_t*     Allocator = &TestAlloc
 
 #endif // __TEST_ALLOCATOR_H__
