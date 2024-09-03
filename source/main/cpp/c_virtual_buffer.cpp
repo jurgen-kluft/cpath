b360b93a054788eb37a9d5ee8351114a41a2af4d
Jurgen Kluft2 <jurgen@virtuosgames.com>
2024-09-03 18:02:41 +0800

    * cpath
    * adjust to change in cvmem API

--- source/main/cpp/c_virtual_buffer.cpp
+++ source/main/cpp/c_virtual_buffer.cpp
@@ -5,7 +5,7 @@ namespace ncore
 {
     void virtual_buffer_t::init(u32 initial_capacity, u32 max_capacity, u32 item_size)
     {
-        u32 const page_size = vmem_t::page_size();
+        u32 const page_size = nvmem::page_size();
 
         m_ptr                    = nullptr;
         u32 const reserved_size  = ((max_capacity * item_size + page_size - 1) / page_size) * page_size;
@@ -13,12 +13,12 @@ namespace ncore
 
         // u64 address_range, u32& page_size, u32 reserve_flags, void*& baseptr
         void* baseptr = nullptr;
-        vmem_t::reserve(reserved_size, {vmem_protect_t::ReadWrite}, baseptr);
+        nvmem::reserve(reserved_size, nvmem::ReadWrite, baseptr);
         m_ptr = (u8*)baseptr;
 
         if (m_ptr && committed_size > 0)
         {
-            vmem_t::commit(m_ptr, committed_size);
+            nvmem::commit(m_ptr, committed_size);
             m_committed = committed_size / item_size;
         }
         m_reserved = reserved_size / item_size;
@@ -26,7 +26,7 @@ namespace ncore
 
     void virtual_buffer_t::exit()
     {
-        vmem_t::release(m_ptr, m_reserved);
+        nvmem::release(m_ptr, m_reserved);
         m_ptr       = nullptr;
         m_reserved  = 0;
         m_committed = 0;
@@ -35,14 +35,14 @@ namespace ncore
     void virtual_buffer_t::reset(u32 initial_capacity, u32 item_size)
     {
         // Figure out how much memory we need to uncommit
-        u32 const page_size      = vmem_t::page_size();
+        u32 const page_size      = nvmem::page_size();
         u32 const committed_size = m_committed * item_size;
         u32 const initial_size   = ((initial_capacity * item_size + page_size - 1) / page_size) * page_size;
         u32 const uncommit_size  = committed_size - initial_size;
 
         if (uncommit_size > 0)
         {
-            vmem_t::decommit(m_ptr + initial_size, uncommit_size);
+            nvmem::decommit(m_ptr + initial_size, uncommit_size);
             m_committed = initial_capacity;
         }
     }
@@ -50,12 +50,12 @@ namespace ncore
     void virtual_buffer_t::add_capacity(u32 capacity, u32 item_size)
     {
         // commit size = capacity * item size
-        u32 const page_size   = vmem_t::page_size();
+        u32 const page_size   = nvmem::page_size();
         u32 const commit_size = ((capacity * item_size + page_size - 1) / page_size) * page_size;
 
         if (commit_size > 0)
         {
-            vmem_t::commit(m_ptr + m_committed * item_size, commit_size);
+            nvmem::commit(m_ptr + m_committed * item_size, commit_size);
             m_committed += commit_size / item_size;
         }
     }
